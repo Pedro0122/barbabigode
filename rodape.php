d666d47210caf077558caa27821c3e38458a3f0b
Pedro0122 <150867648+Pedro0122@users.noreply.github.com>
2023-11-16 15:49:47 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <div>
-        <p>&copy; 2023 by Pedro Bispo,Gloria Maria,Bianca,Yasmin (ITI31).</p>
+        <p>&copy; 2023 by Pedro Bispo,Gloria Maria,Bianca,Yasmin,Gabrielly (ITI31).</p>
         <ul>
             <li>
                 <a href="#" id="twitter">twitter</a>
